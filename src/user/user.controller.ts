60222b84595aee68580832fb5fd53fccdf6249b5
Burak Tosun <61325408+buraktosun@users.noreply.github.com>
2022-11-15 22:26:56 +0300

    gurad-added

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { JwtGuard } from 'src/auth/guard/jwt.guard';
+import { UserDetails } from './user-details.interface';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private userService: UserService){}
+
+    @UseGuards(JwtGuard)
+    @Get(':id')
+    getUser(@Param('id') email : string):Promise<UserDetails | null>{
+        return this.userService.findById(email);
+    }
+    
+}
